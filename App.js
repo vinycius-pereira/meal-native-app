efe11292c2d42d5c2df588f898041410e6d9eeca
Vinícius Pereira <vinicius.pereira@waybee.com.br>
2023-04-27 21:04:18 -0300

    added drawer navigator

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Categories from "./screens/Categories";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import MealsOverview from "./screens/MealsOverview";
 import MealRecipe from "./screens/MealRecipe";
 
+const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Categories" component={Categories} />
+    </Drawer.Navigator>
+  );
+}
 export default function App() {
   return (
     <>
@@ -20,17 +30,11 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="Categories"
-            component={Categories}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{ title: "All Categories" }}
           />
-          <Stack.Screen
-            name="MealsOverview"
-            component={MealsOverview}
-            // options={({ route, navigation }) => {
-            //   return {};
-            // }}
-          />
+          <Stack.Screen name="MealsOverview" component={MealsOverview} />
           <Stack.Screen name="MealRecipe" component={MealRecipe} />
         </Stack.Navigator>
       </NavigationContainer>
